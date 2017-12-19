e5b3c0ee69d6425e8d4e23d82d52dc2284a17005
Clauber Camilo <clauber_camilo@live.com>
2017-12-19 18:52:03 -0200

    :lipstick: Update styles of pagination

--- src/app/components/MyPagination.vue
+++ src/app/components/MyPagination.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="pagination">
+    <div class="my-pagination">
         <pagination
             :records="count"
             :per-page="20"
             @paginate="setPage"
+            class="my-pagination__content"
         ></pagination>
     </div>
 </template>
@@ -38,8 +39,41 @@
 
 </script>
 
-<style lang="scss" scoped>
-    .pagination {
+<style lang="scss">
+    @import '~_scss_config/vars';
 
+    .VuePagination {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-bottom: 20px;
+
+        &__pagination {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+
+            &-item {
+                width: 35px;
+                height: 35px;
+
+                background-color: $primary;
+                color: $white;
+                box-shadow: $raised;
+
+                margin: 10px;
+                border-radius: 2px;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+                transition: background-color .2s ease;
+
+                &:hover {
+                    background-color: lighten($primary, 10)
+                }
+            }
+        }
     }
 </style>
--- src/app/components/PokeList.vue
+++ src/app/components/PokeList.vue
@@ -3,8 +3,6 @@
         <div class="poke-list__content">
             <template v-if="pokemons">
                 <poke v-for="(poke, index) in pokemons" :key="index" :poke="poke"></poke>
-
-
             </template>
 
         </div>
@@ -12,11 +10,16 @@
         <div v-if="pokemons" class="poke-list__pagination">
             <my-pagination></my-pagination>
         </div>
+
+        <div class="poke-list__overlay" v-if="isLoading">
+            <ui-progress-circular :size="50" color="white" class="poke-list__overlay__progress"></ui-progress-circular>
+        </div>
     </div>
 </template>
 
 <script>
     import { mapActions } from 'vuex'
+    import { UiProgressCircular } from 'keen-ui'
     import Poke from '_components/Poke'
     import MyPagination from '_components/MyPagination'
 
@@ -30,11 +33,15 @@
         computed: {
             pokemons () {
                 return this.$store.state.poke.list
+            },
+            isLoading () {
+                return this.$store.state.toggle.loading
             }
         },
         components: {
             MyPagination,
-            Poke
+            Poke,
+            UiProgressCircular
         },
         watch: {
             $route () {
@@ -54,15 +61,37 @@
 </script>
 
 <style lang="scss" scoped>
+    @import '~_scss_config/vars';
+
     .poke-list {
 
         &__content {
             display: flex;
             flex-wrap: wrap;
+            justify-content: center;
         }
 
         &__pagination {
             margin-top: 20px;
+        }
+
+        &__overlay {
+            position: fixed;
+            top: 0;
+            left: 0;
+
+            width: 100vw;
+            height: 100vh;
+
+            background-color: $primary;
+            opacity: .5;
+
+            &__progress {
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+            }
         }
     }
 </style>
